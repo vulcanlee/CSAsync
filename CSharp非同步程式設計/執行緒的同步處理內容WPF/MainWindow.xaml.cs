f1fe4257ec899db7e00f475f49158071b5468ba4
李進興 Vulcan <vulcan@miniasp.com>
2018-08-21 20:24:04 +0800

    在 WPF 使用 執行緒的同步內容 SynchronizationContext 進行非同步工作處理問題

---
+++ CSharp非同步程式設計/執行緒的同步處理內容WPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 執行緒的同步處理內容WPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRunAsyncWithoutSynchronizationContext_Click(object sender, RoutedEventArgs e)
+        {
+            SynchronizationContext BeforeSynchronizationContext = SynchronizationContext.Current;
+            int BeforeThreadID = Thread.CurrentThread.ManagedThreadId;
+            var foo = await new System.Net.Http.HttpClient().GetStringAsync("http://www.google.com").ConfigureAwait(false);
+            SynchronizationContext AfterSynchronizationContext = SynchronizationContext.Current;
+            int AfterThreadID = Thread.CurrentThread.ManagedThreadId;
+            tbkResult.Text = foo;
+        }
+
+        private async void btnRunAsyncWithSynchronizationContext_Click(object sender, RoutedEventArgs e)
+        {
+            SynchronizationContext BeforeSynchronizationContext = SynchronizationContext.Current;
+            int BeforeThreadID = Thread.CurrentThread.ManagedThreadId;
+            var foo = await new System.Net.Http.HttpClient().GetStringAsync("http://www.google.com");
+            SynchronizationContext AfterSynchronizationContext = SynchronizationContext.Current;
+            int AfterThreadID = Thread.CurrentThread.ManagedThreadId;
+            tbkResult.Text = foo;
+        }
+    }
+}
