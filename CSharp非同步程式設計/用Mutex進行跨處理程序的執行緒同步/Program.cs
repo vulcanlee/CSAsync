c9f308f0a998e3c951a3c8146bdaa1a6ae58f50f
李進興 Vulcan <vulcan@miniasp.com>
2018-11-04 21:21:07 +0800

    031 用Mutex進行跨處理程序的執行緒同步 , 030 用Mutex限制只有一個處理程序可以執行

---
+++ CSharp非同步程式設計/用Mutex進行跨處理程序的執行緒同步/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 用Mutex進行跨處理程序的執行緒同步
+{
+    class Program
+    {
+        private static string MutexName = "com.miniasp.process.Mutex";
+        private static Mutex mutex;
+        private static string ProcessName;
+        private static int Sleep;
+
+        public static void Main()
+        {
+            if (!Mutex.TryOpenExisting(MutexName, out mutex))
+            {
+                Console.WriteLine("得到 Mutex");
+                mutex = new Mutex(false, MutexName);
+                ProcessName = "Master 執行程序";
+                Sleep = 3000;
+            }
+            else
+            {
+                ProcessName = "Slave 執行程序";
+                Sleep = 1000;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                mutex.WaitOne();
+                Console.WriteLine($"{ProcessName} 執行時間需要 {Sleep} ms");
+                Thread.Sleep(Sleep);
+                mutex.ReleaseMutex();
+            }
+        }
+
+    }
+}
---
+++ CSharp非同步程式設計/用Mutex限制只有一個處理程序可以執行/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 用Mutex限制只有一個處理程序可以執行
+{
+    class Program
+    {
+        private static string MutexName = "com.miniasp.process.Mutex";
+        private static Mutex mutex;
+        public static void Main()
+        {
+            if (!Mutex.TryOpenExisting(MutexName, out mutex))
+            {
+                mutex = new Mutex(false, MutexName);
+                Console.WriteLine("該程式將會於5秒鐘後自動結束");
+                Thread.Sleep(5000);
+            }
+            else
+            {
+                Console.WriteLine($"已經有相同的程序啟動了，無法再度執行");
+                return;
+            }
+        }
+
+    }
+}
