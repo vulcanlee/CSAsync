e5cc27b9876ab61172b6c15092145515630cf2e1
李進興 Vulcan <vulcan@miniasp.com>
2018-11-04 15:22:13 +0800

    016 獨佔鎖定Monitor 017 lock陳述式的深層鎖定問題

---
+++ CSharp非同步程式設計/lock陳述式的深層鎖定問題/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lock陳述式的深層鎖定問題
+{
+    /// <summary>
+    /// 這個範例將會展示對於同一個鎖定物件，進行深層多此鎖定的練習，
+    /// lock 陳述式知道哪個執行緒正在進行鎖定狀態，若同一個執行緒又執行一次 lock 陳述式，
+    /// 這只是增加鎖定物件的鎖定計數器。
+    /// 在這個範例中，背景工作執行緒需要等到主執行緒最外層 lock 陳述式執行完畢後，才會接續執行
+    /// </summary>
+    class Program
+    {
+        private static int counter = 0;
+        private static object locker = new object();
+        public static void Main()
+        {
+            new Thread(DoWork1).Start();
+            Console.WriteLine($"主執行緒 準備進入到獨佔鎖定狀態 lock (locker)");
+            lock (locker)
+            {
+                Console.WriteLine($"主執行緒 模擬要執行1秒鐘的時間");
+                Thread.Sleep(1000);
+                Console.WriteLine($"主執行緒 第二次準備進入到獨佔鎖定狀態 lock (locker)");
+                lock (locker)
+                {
+                    Console.WriteLine($"主執行緒 模擬要執行1秒鐘的時間");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine($"主執行緒 第一次鎖定準備要結束，模擬要執行5秒鐘的時間");
+                Thread.Sleep(5000);
+                Console.WriteLine($"主執行緒 第一次鎖定正要結束");
+            }
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private static void DoWork1()
+        {
+            Console.WriteLine($"@ 工作執行緒 準備進入到獨佔鎖定狀態 lock (locker)");
+            lock (locker)
+            {
+                Console.WriteLine($"@ 工作執行緒 模擬要執行3秒鐘的時間");
+                Thread.Sleep(3000);
+            }
+            Console.WriteLine($"@ 工作執行緒 鎖定正要結束");
+        }
+    }
+}
---
+++ CSharp非同步程式設計/獨佔鎖定Monitor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 獨佔鎖定Monitor
+{
+    /// <summary>
+    /// 這個範例將會說明當使用獨佔鎖定 Monitor ，如何使用 Monitor.Wait / Monitor.PulseAll 在不同執行緒間的同步執行工作
+    /// </summary>
+    class Program
+    {
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(MyMethod);
+            // 這裡需要等候一段時間，讓工作執行緒已經準備與開始執行了
+            Thread.Sleep(300);
+            Console.WriteLine($"主執行緒 準備進入到獨佔鎖定狀態 lock (locker)");
+            lock (locker) 
+            {
+                Console.WriteLine($"主執行緒 發出 PulseAll 訊號  Monitor.PulseAll(locker)");
+                Monitor.PulseAll(locker);
+                Console.WriteLine($"主執行緒 使用 Wait 等候訊號通知  Monitor.Wait(locker)");
+                Monitor.Wait(locker);
+                Console.WriteLine($"主執行緒 收到來自工作執行緒訊號通知 ");
+            }
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private static void MyMethod(object state)
+        {
+            Console.WriteLine($"@ 工作執行緒 準備進入獨佔鎖定狀態 lock (locker)");
+            lock (locker) 
+            {
+                while (true)
+                {
+                    Console.WriteLine($"@ 工作執行緒 等候 主執行緒 的訊號通知 Monitor.Wait(locker)");
+                    Monitor.Wait(locker); 
+                    Console.WriteLine($"@ 工作執行緒 模擬需要執行");
+                    Console.WriteLine($"@ 工作執行緒 發出 PulseAll 訊號 Monitor.PulseAll(locker)");
+                    Monitor.PulseAll(locker); 
+                }
+            }
+        }
+    }
+}
