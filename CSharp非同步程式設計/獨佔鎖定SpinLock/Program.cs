900c942fce76e720c11d8e8e8b4658722f266cf5
李進興 Vulcan <vulcan@miniasp.com>
2018-11-04 18:11:39 +0800

    2.執行緒 / 028 獨佔鎖定SpinLock

--- CSharp非同步程式設計/獨佔鎖定SpinLock/Program.cs
+++ CSharp非同步程式設計/獨佔鎖定SpinLock/Program.cs
@@ -5,16 +5,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace 獨佔鎖定Mutex
+namespace 獨佔鎖定SpinLock
 {
     class Program
     {
-        private static Mutex mut = new Mutex();
+        private static SpinLock spinLock = new SpinLock();
         private const int numThreads = 3;
 
         static void Main(string[] args)
         {
-           // 產生多個執行緒，執行同一個方法
+            // 產生多個執行緒，執行同一個方法
             for (int i = 0; i < numThreads; i++)
             {
                 ThreadPool.QueueUserWorkItem(ThreadProc);
@@ -27,18 +27,19 @@ namespace 獨佔鎖定Mutex
 
         private static void ThreadProc(object state)
         {
-            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} 等候取得 Mutex 擁有權 WaitOne()");
-            mut.WaitOne();
+            bool lockTaken = false;
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} 等候取得 SpinLock 擁有權 Enter");
+            spinLock.Enter(ref lockTaken);
 
-            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} @ 已經取得 Mutex 擁有權");
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} @ 已經取得 SpinLock 擁有權");
 
             Console.WriteLine($"模擬 Thread{Thread.CurrentThread.ManagedThreadId} 執行專屬獨享的1秒時間的工作");
             Thread.Sleep(1000);
 
 
-            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} 準備要釋放 Mutex 擁有權");
-            mut.ReleaseMutex();
-            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} # 已經要釋放 Mutex 擁有權 ReleaseMutex()");
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} 準備要釋放 SpinLock 擁有權");
+            spinLock.Exit();
+            Console.WriteLine($"Thread{Thread.CurrentThread.ManagedThreadId} # 已經要釋放 SpinLock 擁有權 Exit");
         }
     }
 }
