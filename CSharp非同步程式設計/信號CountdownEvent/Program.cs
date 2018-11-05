4309e5a0e753f72940b29033e0c3d35f671c911d
李進興 Vulcan <vulcan@miniasp.com>
2018-11-05 11:26:26 +0800

    035 信號CountdownEvent

---
+++ CSharp非同步程式設計/信號CountdownEvent/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 信號CountdownEvent
+{
+    class Program
+    {
+        private static CountdownEvent MySyncEvent = new CountdownEvent(4);
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                ThreadPool.QueueUserWorkItem(MyMethod, i);
+            }
+
+            Console.WriteLine("等候四個執行緒執行完畢");
+            MySyncEvent.Wait();
+            Console.WriteLine("四個執行緒已經執行完成");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private static void MyMethod(object state)
+        {
+            Console.WriteLine($"@ 工作執行緒{state} 模擬需要執行");
+            Thread.Sleep(1000);
+            MySyncEvent.Signal();
+        }
+    }
+}
