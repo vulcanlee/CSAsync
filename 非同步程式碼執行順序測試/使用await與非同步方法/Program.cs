4465bae695ae0f3d4837c35d20e7a6ef4a344dc2
李進興 Vulcan <vulcan@miniasp.com>
2019-01-18 00:51:17 +0800

    了解使用 await 與非同步方法執行順序與執行緒的變化

---
+++ 非同步程式碼執行順序測試/使用await與非同步方法/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 使用await與非同步方法
+{
+    // 請試著說出程式碼執行順序，也就是 Checkpoint 與 執行緒 ID 的變化
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            WriteThreadId(1);
+            await LogAsync("Program started");
+
+            WriteThreadId(6);
+            Console.WriteLine($"模擬主程式正在忙碌中，約3秒鐘");
+            //模擬主程式正在忙碌中
+            Thread.Sleep(3000);
+            WriteThreadId(7);
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+
+        private async static Task LogAsync(string @event)
+        {
+            WriteThreadId(2);
+            await Task.Run(() =>
+            {
+                WriteThreadId(3);
+                Console.WriteLine($"模擬寫到資料庫內，約1秒鐘");
+                Thread.Sleep(1000); // 模擬寫到資料庫內
+                WriteThreadId(4);
+
+                Console.WriteLine($"模擬寫到檔案內，約1秒鐘");
+                Thread.Sleep(1000); // 模擬寫到檔案內
+                WriteThreadId(5);
+            });
+
+        }
+
+        private static void WriteThreadId(int checkpoint)
+        {
+            Console.WriteLine("Checkpoint: {0}, thread: {1}", checkpoint, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
